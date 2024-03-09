8f261b7dcddecf0a0a1095f1bc19d719769d52cd
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-03-09 00:58:17 -0300

    ci/ configuring authentication in the container

--- src/MotorAprovacao.WebApi/Program.cs
+++ src/MotorAprovacao.WebApi/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 namespace MotorAprovacao.WebApi
 {
     public class Program
@@ -14,6 +18,33 @@ namespace MotorAprovacao.WebApi
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            //Configuração de autenticação 
+            var secretKey = builder.Configuration["JWT:SecretKey"]
+                            ?? throw new ArgumentException("Invalid secret key!");
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidAudience = builder.Configuration["JWT:ValidAudience"],
+                    ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                                            Encoding.UTF8.GetBytes(secretKey))
+
+                };
+            });
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
