a52718a5cc27632882b6845cb1846002d0e070c2
little-junior <rampagedolol@outlook.com>
2024-03-09 13:18:11 -0300

    feat: add Category class

---
+++ src/MotorAprovacao.Models/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorAprovacao.Models.Entities
+{
+    public class Category
+    {
+        public Category() { }
+
+        public int Id { get; set; }
+        public string Name { get; private set; }
+        public decimal MaximumApproval { get; private set; }
+        public decimal MinimalDisapproval { get; private set; }
+    }
+}
