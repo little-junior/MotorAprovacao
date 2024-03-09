5b44122070ccbef53a46069571f070104d48b582
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-03-09 02:02:27 -0300

    feat/ service creation for token

---
+++ src/MotorAprovacao.WebApi/AuthServices/ITokenService.cs
@@ -0,0 +1,16 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace MotorAprovacao.WebApi.AuthServices
+{
+    public interface ITokenService
+    {
+        JwtSecurityToken GenerateAccessToken(IEnumerable<Claim> claims,
+                                             IConfiguration _config);
+
+        string GenerateRefreshToken();
+
+        ClaimsPrincipal GetPricipalFromExpiredToken(string token,
+                                                    IConfiguration _config);
+    }
+}
