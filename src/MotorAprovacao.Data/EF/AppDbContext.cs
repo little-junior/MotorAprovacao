164117b44703c61b442b2801a0469dc4f534bd54
Luana <luhyxi@gmail.com>
2024-03-09 12:38:37 -0300

    BREAKING CHANGE: Brought the old files to the new repo

---
+++ src/MotorAprovacao.Data/EF/AppDbContext.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MotorAprovacao.Data.EF;
+
+public class AppDbContext : DbContext
+{
+    const int MaxCharsByDocumentoDescription = 200;
+    const int MaxCharsByCategory = 70;
+    const int MaxCharsByAprovadorName = 40;
+    const int MaxIntByAprovacao = 10000;
+    const int MinIntByReprovacao = 10000;
+
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<DocumentoReembolso> ReturnDocuments { get; set; }
+
+    public DbSet<RegrasCategoria> Rules { get; set; }
+    public DbSet<Approver> Approvers { get; set; }
+    public DbSet<Category> Categories { get; set; }
+
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<DocumentoReembolso>(builder =>
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Descricao).HasMaxLength(MaxCharsByDocumentoDescription);
+            builder.Property(s => s.Categoria).HasMaxLength(MaxCharsByCategory);
+            builder.Property(s => s.Status).IsRequired();
+            builder.Property(s => s.Total).IsRequired().HasPrecision(5);
+
+            builder.HasOne(s => Approvers);
+        });
+
+        modelBuilder.Entity<RegrasCategoria>(builder =>
+        {
+            builder.HasKey(s => s.Id);
+            builder.Property(s => s.Categoria).HasMaxLength(MaxCharsByCategory);
+            builder.Property(s => s.MaximoAprovacao).HasMaxLength(MaxIntByAprovacao);
+            builder.Property(s => s.MinimoReprovacao).HasMaxLength(MinIntByReprovacao);
+
+        });
+
+        modelBuilder.Entity<Approver>(builder =>
+        {
+            builder.HasKey(s => s.ApproverId);
+            builder.Property(s => s.ApproverName).HasMaxLength(MaxCharsByAprovadorName);
+            builder.Property(s => s.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+        });
+
+        modelBuilder.Entity<Category>(builder =>
+        {
+            builder.HasKey(s => s.CategoryId);
+            builder.Property(s => s.CategoryName).HasMaxLength(MaxCharsByAprovadorName);
+            builder.Property(s => s.CreatedAt).IsRequired().ValueGeneratedOnAdd();
+        });
+    }
+
+}
---
+++ src/MotorAprovacao.Data/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace MotorAprovacaoME.Data.Models;
+
+public class Category
+{
+    public Category()
+    {
+        CreatedAt = DateTime.Now;
+    }
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; }
+    internal DateTime CreatedAt { get; set; }
+}
