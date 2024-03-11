5e3aaa760f1e38fe5355ae830c73cf3090e3322b
Monalisa Freitas de Brito <monalisabrito.ads@gmail.com>
2024-03-11 12:11:35 -0300

    feat/creation of security-related DTOs

---
+++ src/MotorAprovacao.WebApi/AuthDTOs/LoginDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorAprovacao.WebApi.AuthDTOs
+{
+    public class LoginDTO
+    {
+        [Required(ErrorMessage = "Nome do Usuario é obrigatorio!")]
+        public string? UserName { get; set; }
+
+
+        [Required(ErrorMessage = "A senha é obrigatorio!")]
+        public string? Password { get; set; }
+    }
+}
---
+++ src/MotorAprovacao.WebApi/AuthDTOs/TokenDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace MotorAprovacao.WebApi.AuthDTOs
+{
+    public class TokenDTO
+    {
+        public string? AcessToken { get; set; }
+        public string? RefreshToken { get; set; }
+
+    }
+}
