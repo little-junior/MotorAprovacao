7280d708aee3b7b4d4c449564cd6111e1bdd5a5f
little-junior <rampagedolol@outlook.com>
2024-03-11 18:05:17 -0300

    feat: add RefundDocument interface repository

---
+++ src/MotorAprovacao.Data/Repositories/IRefundDocumentRepository.cs
@@ -0,0 +1,19 @@
+﻿using MotorAprovacao.Models.Entities;
+using MotorAprovacao.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorAprovacao.Data.Repositories
+{
+    public interface IRefundDocumentRepository
+    {
+        Task<RefundDocument> GetById(Guid id);
+        Task<IEnumerable<RefundDocument>> GetByStatus(Status status);
+        Task Add(RefundDocument document);
+        Task Update(RefundDocument document);
+        Task<bool> SaveChanges();
+    }
+}
