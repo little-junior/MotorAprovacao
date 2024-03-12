81c5d1b2c413f911cbe69a2b378a0dacb0f9a11d
little-junior <rampagedolol@outlook.com>
2024-03-11 22:05:37 -0300

    feat: add RefundDocument request dto

---
+++ src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotorAprovacao.WebApi.RequestDtos
+{
+    public class RefundDocumentRequestDto
+    {
+        [Required]
+        public decimal Total { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public string Description { get; set; }
+
+    }
+}
