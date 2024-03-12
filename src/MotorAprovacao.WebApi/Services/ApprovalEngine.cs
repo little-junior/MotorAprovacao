f5c331df9c5af41ef8dde24d8ff7f04c2aad94d6
little-junior <rampagedolol@outlook.com>
2024-03-11 22:06:37 -0300

    feat: add ApprovalEngine implementation

---
+++ src/MotorAprovacao.WebApi/Services/ApprovalEngine.cs
@@ -0,0 +1,47 @@
+﻿using MotorAprovacao.Models.Entities;
+
+namespace MotorAprovacao.WebApi.Services
+{
+    public class ApprovalEngine : IApprovalEngine
+    {
+        private readonly AppDbContext _context;
+
+        public ApprovalEngine(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task ProcessDocument(RefundDocument document)
+        {
+            decimal maxApprovalCategory, minDisapprovalCategory;
+
+            var categoryRules = await _context.Rules.FirstOrDefaultAsync(rule => rule.CategoryId == document.CategoryId);
+
+            if (categoryRules != null)
+            {
+                maxApprovalCategory = categoryRules.MaximumApproval;
+                minDisapprovalCategory = categoryRules.MinimalDisapproval;
+            }
+            else
+            {
+                var defaultCategoryRules = await _context.Rules.FirstOrDefaultAsync(rule => rule.CategoryId == 1);
+                maxApprovalCategory = defaultCategoryRules.MaximumApproval;
+                minDisapprovalCategory = defaultCategoryRules.MinimalDisapproval;
+            }
+
+            decimal requestTotal = document.Total;
+
+            if (requestTotal <= maxApprovalCategory)
+            {
+                document.Approve();
+                return;
+            }
+
+            if (requestTotal > minDisapprovalCategory)
+            {
+                document.Disapprove();
+                return;
+            }
+        }
+    }
+}
