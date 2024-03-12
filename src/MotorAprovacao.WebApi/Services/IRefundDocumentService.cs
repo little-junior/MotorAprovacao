001ec6286b3f719155fb596c52f2fadd5b8795fe
little-junior <rampagedolol@outlook.com>
2024-03-11 22:05:58 -0300

    feat: add RefundDocument service interface

---
+++ src/MotorAprovacao.WebApi/Services/IRefundDocumentService.cs
@@ -0,0 +1,14 @@
+﻿using MotorAprovacao.Models.Entities;
+using MotorAprovacao.Models.Enums;
+using MotorAprovacao.WebApi.RequestDtos;
+
+namespace MotorAprovacao.WebApi.Services
+{
+    public interface IRefundDocumentService
+    {
+        Task<IEnumerable<RefundDocument>> GetDocumentsByStatus(Status status);
+        Task<RefundDocument> CreateDocument(RefundDocumentRequestDto documentDto);
+        Task ApproveDocument(Guid id);
+        Task DisapproveDocument(Guid id);
+    }
+}
