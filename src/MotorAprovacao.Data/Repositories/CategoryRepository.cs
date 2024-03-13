1ae41c99b39812e519aa3baeca80c9ac5a909955
little-junior <rampagedolol@outlook.com>
2024-03-13 20:13:34 -0300

    feat: add CategoryRepository implementation

---
+++ src/MotorAprovacao.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorAprovacao.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorAprovacao.Data.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly AppDbContext _context;
+        public CategoryRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CheckExistenceById(int id)
+        {
+            return await _context.Categories.AnyAsync(c => c.Id == id);
+        }
+    }
+}
