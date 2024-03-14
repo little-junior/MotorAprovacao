ed7d23b62b96e0f7a559ce014d1d90dbffcd273b
Pedr0Siqueira <pedro_jsiqueira@hotmail.com>
2024-03-14 04:18:22 -0300

    some more tests

--- tests/MotorAprovacao.Tests/ApprovalEngineTests.cs
+++ tests/MotorAprovacao.Tests/ApprovalEngineTests.cs
@@ -39,19 +39,31 @@ namespace MotorAprovacao.Tests
             _repoMock = new RefundDocumentRepository(_context);
             _documentService = new RefundDocumentService(_approvalEngineMock, _repoMock);
 
+
             TestDataSeeder.SeedData(_context);
         }
 
-        [Theory]
-        [ClassData(typeof(RefundDocumentRequestDtoShouldAproveBelow))]
-        public async Task AprovarReembolso_ComRegraDeAprovação_AcimaDoLimiteDeValor_DeveAprovar(decimal total, int categoryId, string description)
+        [Fact]
+        public async Task AprovarReembolso_ComRegraDeAprovação_AcimaDoLimiteDeValor_DeveAprovar_Categoria2()
         {
             // Arrange
+            Setup();
+
+            var request = new RefundDocumentRequestDto
+            {
+                Total = 1200m,
+                CategoryId = 2, // Usando CategoryId correto para a categoria 2
+                Description = "Title"
+            };
 
             // Act
+            var document = await _documentService.CreateDocument(request);
+            await _approvalEngineMock.ProcessDocument(document);
 
             // Assert
+            document.Status.Should().Be(Status.Approved);
         }
+
 
 
         [Theory]
@@ -102,31 +114,64 @@ namespace MotorAprovacao.Tests
         public async Task RecusarReembolso_ComRegraDeRecusa_TodasAsCategorias_DeveRecusar()
         {
             // Arrange
+            Setup(); // Configuração do ambiente de teste
+
+            var request = new RefundDocumentRequestDto
+            {
+                Total = 1200m, 
+                CategoryId = 1,
+                Description = "Title"
+            };
 
             // Act
+            var document = await _documentService.CreateDocument(request);
 
             // Assert
+            document.Status.Should().Be(Status.Disapproved); 
         }
+
+
 
         [Fact]
         public async Task AprovarReembolso_SemRegrasEspecificas_DeveAprovar()
         {
             // Arrange
+            Setup();
+            var request = new RefundDocumentRequestDto
+            {
+                Total = 800m,
+                CategoryId = 4,
+                Description = "Title"
+            };
 
             // Act
+            var document = await _documentService.CreateDocument(request);
+            await _approvalEngineMock.ProcessDocument(document);
 
             // Assert
+            document.Status.Should().Be(Status.Approved);
         }
+
 
 
         [Fact]
         public async Task RecusarReembolso_ComRegraDeRecusa_Transporte_DeveRecusar()
         {
             // Arrange
+            Setup();
+            var request = new RefundDocumentRequestDto
+            {
+                Total = 1200m,
+                CategoryId = 3,
+                Description = "Title"
+            };
 
             // Act
+            var document = await _documentService.CreateDocument(request);
+            await _approvalEngineMock.ProcessDocument(document);
 
             // Assert
+            document.Status.Should().Be(Status.Disapproved);
         }
 
 
@@ -134,21 +179,41 @@ namespace MotorAprovacao.Tests
         public async Task CriarReembolso_ComDadosValidos_EstadoInicial_DeveSalvarNoBancoDeDados()
         {
             // Arrange
+            Setup();
+            var request = new RefundDocumentRequestDto
+            {
+                Total = 200m,
+                CategoryId = 1,
+                Description = "Title"
+            };
 
             // Act
+            var document = await _documentService.CreateDocument(request);
 
             // Assert
+            document.Should().NotBeNull();
+            document.Status.Should().Be(Status.OnApproval);
+            // aqui ficou faltando verificar se o id foi gerado pelo dt
         }
-
 
         [Fact]
         public async Task CriarReembolso_ComDadosInvalidos_DeveRetornarErro()
         {
             // Arrange
+            Setup();
+            var request = new RefundDocumentRequestDto
+            {
+                Total = -100m,
+                CategoryId = 10,
+                Description = "Title"
+            };
 
             // Act
+            Func<Task> createDocumentAction = async () => await _documentService.CreateDocument(request);
 
             // Assert
+            await createDocumentAction.Should().ThrowAsync<Exception>();
         }
+
     }
 }
