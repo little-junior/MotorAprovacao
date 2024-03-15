b95d9d8ea2d6700e48926ae75e4ebb19fe905d09
little-junior <rampagedolol@outlook.com>
2024-03-14 22:18:05 -0300

    feat: add ValidationActionFilter implementation

---
+++ src/MotorAprovacao.WebApi/Filters/ValidationActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using MotorAprovacao.WebApi.ErrorHandlers;
+
+namespace MotorAprovacao.WebApi.Filters
+{
+    public class ValidationActionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                context.Result = new BadRequestObjectResult(context.ModelState);
+            }
+        }
+    }
+}
