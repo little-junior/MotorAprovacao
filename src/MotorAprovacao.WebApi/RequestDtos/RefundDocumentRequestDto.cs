6889704870d068e9e6d0cdd6d0607c62640729ed
little-junior <rampagedolol@outlook.com>
2024-03-15 15:30:27 -0300

    feat: remove DataAnnotation attributes

--- src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
+++ src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
@@ -4,14 +4,10 @@ namespace MotorAprovacao.WebApi.RequestDtos
 {
     public class RefundDocumentRequestDto
     {
-        [Required(ErrorMessage = "The field 'total' is required.")]
         public decimal Total { get; set; }
 
-        [Required(ErrorMessage = "The field 'categoryId' is required.")]
         public int CategoryId { get; set; }
 
-        [Required(ErrorMessage = "The field 'description' is required.")]
-        [StringLength(200, ErrorMessage ="The field must be under 200 characters.")]
         public string Description { get; set; }
     }
 }
