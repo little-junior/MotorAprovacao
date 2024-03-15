d1ef3b423a6938ebf2a870ef65fb09816652084f
little-junior <rampagedolol@outlook.com>
2024-03-14 22:17:50 -0300

    feat: add StringLength attribute to Description

--- src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
+++ src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
@@ -4,12 +4,14 @@ namespace MotorAprovacao.WebApi.RequestDtos
 {
     public class RefundDocumentRequestDto
     {
-        [Required]
+        [Required(ErrorMessage = "The field 'total' is required.")]
         public decimal Total { get; set; }
-        [Required]
-        public int CategoryId { get; set; }
-        [Required]
-        public string Description { get; set; }
 
+        [Required(ErrorMessage = "The field 'categoryId' is required.")]
+        public int CategoryId { get; set; }
+
+        [Required(ErrorMessage = "The field 'description' is required.")]
+        [StringLength(200, ErrorMessage ="The field must be under 200 characters.")]
+        public string Description { get; set; }
     }
 }
