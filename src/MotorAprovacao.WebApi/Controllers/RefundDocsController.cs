b83d2615745fe2609b32e8af22112f271fcd57b4
little-junior <rampagedolol@outlook.com>
2024-03-16 19:03:22 -0300

    feat: add xml comments and ProducesResponseType attributes on actions methods

--- src/MotorAprovacao.WebApi/Controllers/RefundDocsController.cs
+++ src/MotorAprovacao.WebApi/Controllers/RefundDocsController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using MotorAprovacao.Data.Repositories;
+using MotorAprovacao.Models.Entities;
 using MotorAprovacao.Models.Enums;
 using MotorAprovacao.WebApi.ErrorHandlers;
 using MotorAprovacao.WebApi.Filters;
@@ -12,17 +13,30 @@ namespace MotorAprovacao.WebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Produces("application/json")]
     public class RefundDocsController : ControllerBase
     {
         private readonly IRefundDocumentService _service;
         private readonly ICategoryRepository _categoryRepository;
+
         public RefundDocsController(IRefundDocumentService service, ICategoryRepository categoryRepository)
         {
             _service = service;
             _categoryRepository = categoryRepository;
         }
 
+        /// <summary>
+        /// Finds a specific Refund Document by Id
+        /// </summary>
+        /// <param name="id">Id of Refund Document to return</param>
+        /// <returns>A Refund Document</returns>
+        /// <response code="200">Returns the Refund Document</response>
+        /// <response code="404">Refund Document not found</response>
+        /// <response code="400">Id format is invalid</response>
         [HttpGet("{id}")]
+        [ProducesResponseType(typeof(RefundDocumentResponseDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorModelStateResponse), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetById([FromRoute] Guid id)
         {
             var document = await _service.GetDocumentById(id);
@@ -36,8 +50,18 @@ namespace MotorAprovacao.WebApi.Controllers
 
             return Ok(documentResponseDto);
         }
-
+        /// <summary>
+        /// Finds Refund Documents by Status
+        /// </summary>
+        /// <param name="status">Refund Document's Status
+        /// <para>0 - OnApproval, 1 - Approved, 2 - Disapproved</para>
+        /// </param>
+        /// <returns>A list of Refund Documents</returns>
+        /// <response code="200">Returns the list of Refund Documents</response>
+        /// <response code="400">Status value is invalid</response>
         [HttpGet]
+        [ProducesResponseType(typeof(List<RefundDocumentResponseDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> GetByStatus([FromQuery]int status)
         {
             if (!Enum.IsDefined(typeof(Status), status))
@@ -54,8 +78,28 @@ namespace MotorAprovacao.WebApi.Controllers
 
             return Ok(documentsResponseDtos);
         }
-
+        /// <summary>
+        /// Creates a Refund Document
+        /// </summary>
+        /// <remarks>
+        /// Sample request:
+        /// 
+        ///     POST api/RefundDocs
+        ///     {
+        ///         "total": 200,
+        ///         "categoryId": 2,
+        ///         "description": "Document description"
+        ///     }
+        /// </remarks>
+        /// <param name="documentDto">Refund Document request body
+        /// <para>The JSON object representing some fields of the Refund Document</para>
+        /// </param>
+        /// <returns>A new Refund Document</returns>
+        /// <response code="201">Returns the newly created Refund Document</response>
+        /// <response code="400">At least one field in the Refund Document Request Body is invalid</response>
         [HttpPost]
+        [ProducesResponseType(typeof(RefundDocumentResponseDto), StatusCodes.Status201Created)]
+        [ProducesResponseType(typeof(ErrorModelStateResponse), StatusCodes.Status400BadRequest)]
         [TypeFilter(typeof(ValidationActionFilter))]
         public async Task<IActionResult> PostRequestDoc([FromBody] RefundDocumentRequestDto documentDto)
         {
@@ -75,7 +119,21 @@ namespace MotorAprovacao.WebApi.Controllers
             return Created($"api/refunddocs/{documentResponseDto.Id}", documentResponseDto);
         }
 
+        /// <summary>
+        /// Updates a Refund Document by changing its Status to Approved
+        /// </summary>
+        /// <param name="id">Id of Refund Document to update</param>
+        /// <returns></returns>
+        /// <response code="204">Refund Document's Status was updated</response>
+        /// <response code="400">Id format is invalid</response>
+        /// <response code="404">Refund Document not found</response>
+        /// <response code="409">Refund Document could not be updated</response>
         [HttpPatch("{id}/approve")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ErrorModelStateResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status409Conflict)]
+        [ProducesDefaultResponseType(typeof(ErrorResponse))]
         public async Task<IActionResult> PatchApprove([FromRoute] Guid id)
         {
             var document = await _service.GetDocumentById(id);
@@ -94,8 +152,20 @@ namespace MotorAprovacao.WebApi.Controllers
 
             return NoContent();
         }
-
+        /// <summary>
+        /// Updates a Refund Document by changing its Status to Disapproved
+        /// </summary>
+        /// <param name="id">Id of Refund Document to update</param>
+        /// <returns></returns>
+        /// <response code="204">Refund Document's Status was updated</response>
+        /// <response code="400">Id format is invalid</response>
+        /// <response code="404">Refund Document not found</response>
+        /// <response code="409">Refund Document could not be updated</response>
         [HttpPatch("{id}/disapprove")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ErrorModelStateResponse), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ErrorResponse), StatusCodes.Status409Conflict)]
         public async Task<IActionResult> PatchDisapprove([FromRoute] Guid id)
         {
             var document = await _service.GetDocumentById(id);
