bc929eda00ab74bff2ea5ce9b05e3691d8644cd2
little-junior <rampagedolol@outlook.com>
2024-03-16 19:01:20 -0300

    feat: add summary comments on properties

--- src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
+++ src/MotorAprovacao.WebApi/RequestDtos/RefundDocumentRequestDto.cs
@@ -4,10 +4,17 @@ namespace MotorAprovacao.WebApi.RequestDtos
 {
     public class RefundDocumentRequestDto
     {
+        /// <summary>
+        /// The total value of refund
+        /// </summary>
         public decimal Total { get; set; }
-
+        /// <summary>
+        /// Id of a Category
+        /// </summary>
         public int CategoryId { get; set; }
-
+        /// <summary>
+        /// Description of the document
+        /// </summary>
         public string Description { get; set; }
     }
 }
