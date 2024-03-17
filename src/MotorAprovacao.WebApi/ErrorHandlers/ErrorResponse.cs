bf0690fc9b9bd94288293e55d8386a731dfc509c
little-junior <rampagedolol@outlook.com>
2024-03-17 19:32:15 -0300

    refactor: change ErrorResponse to be the main error response

--- src/MotorAprovacao.WebApi/ErrorHandlers/ErrorResponse.cs
+++ src/MotorAprovacao.WebApi/ErrorHandlers/ErrorResponse.cs
@@ -1,16 +1,31 @@
-﻿namespace MotorAprovacao.WebApi.ErrorHandlers
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace MotorAprovacao.WebApi.ErrorHandlers
 {
     public class ErrorResponse
     {
-        public ErrorResponse(int statusCode, string type, string message) 
+        public ErrorResponse(int statusCode, string type, string field, string message)
         {
             StatusCode = statusCode;
             Type = type;
-            Message = message;
+            Messages = new Dictionary<string, string[]>()
+            {
+                {field, new string[] {message} }
+            };
         }
-
+        public ErrorResponse(int statusCode, string type, ModelStateDictionary modelState)
+        {
+            StatusCode = statusCode;
+            Type = type;
+            Messages = modelState
+            .Where(entry => entry.Value.Errors.Any())
+            .ToDictionary(
+                kvp => kvp.Key,
+                kvp => kvp.Value.Errors.Select(error => error.ErrorMessage).ToArray()
+            );
+        }
         public int StatusCode { get; set; }
         public string Type { get; set; }
-        public string Message { get; set; }
+        public IDictionary<string, string[]> Messages { get; set; }
     }
 }
